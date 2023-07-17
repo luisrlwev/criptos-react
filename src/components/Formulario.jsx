52d55ce1888f01131db7fc4dc69c9b7c7b3d61a4
Luis Angel Hernandez <rlwev@hotmail.com>
2023-07-17 17:33:22 -0500

    Agregamos state y retornamos un valor en nuestro hook - clase 143

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -24,7 +24,7 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
 
-    const [ SelectMonedas ] = useSelectMonedas('Elige tu moneda', monedas)
+    const [ moneda, SelectMonedas ] = useSelectMonedas('Elige tu moneda', monedas)
 
   return (
     <form>
