032611b8ee5ce3dd3eb70624d9bfab9cd7bd06dd
Luis Angel Hernandez <rlwev@hotmail.com>
2023-07-17 17:09:33 -0500

    Le pasamos opciones a nuestro Hook y agregamos estilos al select - clase 142

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import useSelectMonedas from '../hooks/useSelectMonedas'
+import { monedas } from '../data/monedas'
 
 const InputSubmit = styled.input`
     background-color: #9497ff;
@@ -15,6 +16,7 @@ const InputSubmit = styled.input`
     letter-spacing: 3.4px;
     cursor: pointer;
     transition: background-color .3s ease;
+    margin-top: 30px;
     &:hover{
         background-color: #7a7dfe;
     }
@@ -22,11 +24,11 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
 
-    const [ SelectMonedas ] = useSelectMonedas()
-    SelectMonedas()
+    const [ SelectMonedas ] = useSelectMonedas('Elige tu moneda', monedas)
 
   return (
     <form>
+        <SelectMonedas />
         <InputSubmit type="submit" value='Cotizar'/>
     </form>
   )
--- src/hooks/useSelectMonedas.jsx
+++ src/hooks/useSelectMonedas.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import styled from '@emotion/styled'
 
-const useSelectMonedas = () => {
+const Label = styled.label`
+    color: #fff;
+    display: block;
+    font-family: 'Poppins', sans-serif;
+    font-size: 24px;
+    font-weight: 700;
+    margin: 15px 0;
+`
 
-    const SelectMonedas = () => {
-        console.log('Desde Select Monedas')
-    }
+const Select = styled.select`
+    width: 100%;
+    font-size: 18px;
+    padding: 14px;
+    border-radius: 7px;
+`
+
+const useSelectMonedas = (label, opciones) => {
+
+    const SelectMonedas = () => (
+        <>
+            <Label>{label}</Label>
+            <Select>
+                <option value="">-- Seleccione --</option>
+                {opciones.map( opcion => (
+                    <option
+                        key={opcion.id}
+                        value={opcion.id}
+                    >{opcion.nombre}</option>
+                ))}
+            </Select>
+        </>
+    )
 
     return [ SelectMonedas ]
 }
