9fe2b3c8c86a1e82a43a999e6a9130c0668fb9e0
Luis Angel Hernandez <rlwev@hotmail.com>
2023-07-17 16:38:46 -0500

    Creamos nuestro primer hook - clase 139

---
+++ src/hooks/useSelectMonedas.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const useSelectMonedas = () => {
+
+    const SelectMonedas = () => {
+        console.log('Desde Select Monedas')
+    }
+
+    return [ SelectMonedas ]
+}
+
+export default useSelectMonedas
