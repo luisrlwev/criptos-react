acd84b5c8dba8298d9b5cbbfb544c487a380f021
Luis Angel Hernandez <rlwev@hotmail.com>
2023-07-19 16:36:37 -0500

    Agregamos el spinner - clase 152

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import styled from '@emotion/styled'
 import Formulario from './components/Formulario'
 import Resultado from './components/Resultado'
+import Spinner from './components/Spinner'
 import ImgCripos from './img/imagen-criptos.png'
 
 const Contenedor = styled.div`
@@ -45,10 +46,15 @@ function App() {
 
   const [monedas, setMonedas] = useState({})
   const [resultado, setResultado] = useState({})
+  const [cargando, setCargando] = useState(false)
 
   useEffect(() => {
     if(Object.keys(monedas).length > 0){
+
       const cotizarCripto = async () => {
+        setCargando(true)
+        setResultado({})
+
         const { moneda, cripto } = monedas
         const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cripto}&tsyms=${moneda}`
 
@@ -56,6 +62,8 @@ function App() {
         const resultado = await respuesta.json()
 
         setResultado(resultado.DISPLAY[cripto][moneda]);
+
+        setCargando(false);
       }
       cotizarCripto();
     }
@@ -72,6 +80,7 @@ function App() {
         <Formulario
           setMonedas={setMonedas}
         />
+        {cargando && <Spinner/>}
         {resultado.PRICE && <Resultado resultado={resultado}/>}
       </div>
     </Contenedor>
